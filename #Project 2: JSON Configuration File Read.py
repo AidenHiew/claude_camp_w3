1cc69c0368ae06b55d08786a6a137d7f1c13bb76
AidenHiew <281902732+AidenHiew@users.noreply.github.com>
2026-05-23 09:43:05 +0930

    debug on line 106~115

--- #Project 2: JSON Configuration File Read.py
+++ #Project 2: JSON Configuration File Read.py
@@ -19,8 +19,27 @@ def load_config():
     
     #if the file exists, load the config
     with open(FILE_NAME, 'r') as files:
-        return json.load(files)
+        config = json.load(files)
     
+    #validate loaded values
+    for key, value in config.items():
+        is_valid, validate_value = validate_setting(key, value)
+        if is_valid:
+            config[key] = validate_value
+        else:
+            print (f"Invalid value found for {key} in config file. Using default value.")
+
+            if key == "font_size":
+                config[key] = 14
+            elif key == "theme":
+                config[key] = "orange"
+            elif key == "language":
+                config[key] = "en"
+    save_config(config)
+    return config
+           
+
+
     
 def save_config(config_data):
     with open (FILE_NAME, 'w') as files:
@@ -38,17 +57,17 @@ def validate_setting(key, value):
                 print("Font size must be between 8 and 32.")
                 return False, None
         except ValueError:
-            print("Font size must be a nuber.")
+            print("Font size must be a number.")
             return False, None
 
     if key == "theme":
-        allowed_thme = ["orange", "blue", "green"]
+        allowed_theme = ["orange", "blue", "green"]
         cleaned_value = value.strip().lower()
         
-        if cleaned_value in allowed_thme:
+        if cleaned_value in allowed_theme:
             return True, cleaned_value
         else:
-            print(f"Please enter from the following options: {', '.join(allowed_thme)}.")
+            print(f"Please enter from the following options: {', '.join(allowed_theme)}.")
             return False, None
 
     if key == "language":
@@ -68,15 +87,14 @@ def main():
     
     while True:
         print("Current configuration:")
-
         for key, value in config.items():
             print(f"{key}: {value}")
     
         print("-------------------------")
     
-        target_setting = input("Enter the setting you want to modify (or 'exit' to quit):")
+        target_setting = input("Enter the setting you want to modify (or 'exit' to quit):").strip().lower()
+        
     
- 
         if target_setting.lower() == "exit":
             print("Exiting the configuration editor.")
             break
@@ -87,30 +105,17 @@ def main():
         
         while True:
             new_value = input(f"Enter the new value for {target_setting}:")
-        is_valid, validated_value = validate_setting(target_setting, new_value)
 
-        if is_valid:
-            config[target_setting] = validated_value
-            save_config(config)
-            break
+            is_valid, validated_value = validate_setting(target_setting, new_value)
 
-            print(f"Invalid value. {validated_value} Please try again.")
+            if is_valid:
+                config[target_setting] = validated_value
+                save_config(config)
+                break
+            print(f"Please pick one from the list and try again.")
 
 if __name__ == "__main__":
     main()
 
 
         
-   
-
-
-#testing
-config = load_config()
-print("Current settings:")
-
-config["theme"] = "blue"
-save_config(config)
-            
-config = load_config()
-print("Updated settings:")
-print(config)
